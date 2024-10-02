1fa598d699eeda3a4ab78efc9c0ab5ab29e79471
yantong <yantong1775@gmail.com>
2024-10-02 16:22:03 -0400

    configuration changes

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://35.232.93.126:80/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
